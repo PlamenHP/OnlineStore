6bb899ba0c2e566a9564b0c28e2606602f02c28b
PlamenHP <p.petrov.mail@gmail.com>
2016-12-11 09:52:48 +0000

    added carousel to home page, logo for the header and glyph icons for account and shopping cart buttons in header layout

--- Source/Data/OnlineStore.Models/ApplicationUser.cs
+++ Source/Data/OnlineStore.Models/ApplicationUser.cs
@@ -1,24 +1,19 @@
 ﻿
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
-using OnlineStore.Common.Models;
 using System.Security.Claims;
 using System.Threading.Tasks;
-using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace OnlineStore.Models
 {
-    public class ApplicationUser : IdentityUser, IAuditInfo, IDeletableEntity
+    public class ApplicationUser : IdentityUser
     {
-        public DateTime CreatedOn { get; set; }
+        [Required]
+        public string FirstName { get; set; }
 
-        public DateTime? DeletedOn { get; set; }
-
-        public bool IsDeleted { get; set; }
-
-        public DateTime? ModifiedOn { get; set; }
-
-        public bool PreserveCreatedOn { get; set; }
+        [Required]
+        public string LastName { get; set; }
 
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {

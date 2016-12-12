7ea2388ff8c501512271de744855d4482b190a8e
PlamenHP <p.petrov.mail@gmail.com>
2016-12-12 23:01:19 +0000

    cleared view engines other than razor

--- Source/Web/OnlineStore.Web/Global.asax.cs
+++ Source/Web/OnlineStore.Web/Global.asax.cs
@@ -12,6 +12,8 @@ namespace OnlineStore.Web
     {
         protected void Application_Start()
         {
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new RazorViewEngine());
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);

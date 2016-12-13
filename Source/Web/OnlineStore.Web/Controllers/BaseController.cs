a21e844dcc96f1e1b18eee4dbf19a400b45d0cfa
PlamenHP <p.petrov.mail@gmail.com>
2016-12-13 01:21:57 +0000

    added base controller

---
+++ Source/Web/OnlineStore.Web/Controllers/BaseController.cs
@@ -0,0 +1,24 @@
+﻿using OnlineStore.Data.UnitOfWork;
+using OnlineStore.Models;
+using System.Web.Mvc;
+
+namespace OnlineStore.Web.Controllers
+{
+    abstract class BaseController : Controller
+    {
+        public BaseController(IStoreDb data)
+        {
+            this.Data = data;
+        }
+
+        public BaseController(IStoreDb data, ApplicationUser storeUser)
+            : this(data)
+        {
+            this.StoreUser = storeUser;
+        }
+
+        protected IStoreDb Data { get; set; }
+
+        protected ApplicationUser StoreUser { get; set; }
+    }
+}

e83b41ffb4419d42623fb4f06ca0b75474c253e3
PlamenHP <p.petrov.mail@gmail.com>
2016-12-14 00:44:27 +0000

    created ViewEnginesConfig and overal code improvements

---
+++ Source/Web/OnlineStore.Web/ViewModels/ProductsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineStore.Web.ViewModels
+{
+    public class ProductsViewModel
+    {
+
+    }
+}
